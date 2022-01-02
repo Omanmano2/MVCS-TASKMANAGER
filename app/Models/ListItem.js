495208097a4bc297c61f53efaebfb264647e5d8e
Owen Manmano <Bigowen27@gmail.com>
2022-01-01 23:49:33 -0700

    push

---
+++ app/Models/ListItem.js
@@ -0,0 +1,24 @@
+import { generateId } from "../Utils/generateId.js"
+
+
+
+
+
+export class ListItem {
+
+  constructor(data) {
+    this.name = data.name
+    this.id = generateId()
+    this.listTemplateId = data.listTemplateId
+  }
+
+  get Template() {
+    return /*html*/ `
+    <li class="py-3 text-even">
+      <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+      <p>${this.name}</p>
+      <i class="mdi mdi-delete selectable" onclick="app.listItemsService.deleteListItem(${this.id})"></i>
+    </li>
+    `
+  }
+}
